5b1df13bb2a8b3a08101dc80aef6e126bfef1049
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-03-13 00:22:49 -0500

    more

--- caesar_cipher_helper.rb
+++ caesar_cipher_helper.rb
@@ -2,25 +2,26 @@ class CaesarCipher
   def initialize(shift)
     @shift = shift
     @alphabet =  Array("A".."Z")
-    p @alphabet
+    @shifted_alphabet = @alphabet.rotate(@shift)
   end
 
   def encode(string)
-    @shifted_alphabet = @alphabet.rotate(@shift)
-    p @shifted_alphabet
     hash = Hash[@alphabet.zip(@shifted_alphabet)]
-    p hash
     array_string = string.upcase.split("")
-    p array_string
-    p result = array_string.map { |letter| hash[letter] }
-    result = array_string.join
-    p result
+    result = array_string.map { |letter| hash[letter] }
+    result = result.join
+    result
   end
   
   def decode(string)
-    #your code here
+    hash = Hash[@shifted_alphabet.zip(@alphabet)]
+    array_string = string.upcase.split("")
+    result = array_string.map { |letter| hash[letter] }
+    result = result.join
+    result
   end
 end
 
 c=CaesarCipher.new(5)
-c.encode('Codewars') #'HTIJBFWX'
+p c.encode('Codewars') #'HTIJBFWX'
+p c.decode('HTIJBFWX') #'CODEWARS
